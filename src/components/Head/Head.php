9d1c1217998e9fb235d15282dd76ce70535c5076
Esteban Zepeda <estzepeda@outlook.com>
2025-03-22 23:10:57 -0600

    Create Head.php

---
+++ src/components/Head/Head.php
@@ -0,0 +1,18 @@
+<head>
+    <!-- General -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="author" content="Qarta Online">
+    <title>ZepCo</title>
+    <meta name="description" content="Zepco">
+    <link rel="canonical" href="https://zepco.qartaonline.com/">
+    <meta name="url" content="https://zepco.qartaonline.com/">
+    
+    <!-- Favicon and icons -->
+    <link rel="icon" href="./favicon.ico">
+
+    <!-- Stylesheets -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
