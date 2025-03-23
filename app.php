4a9745bc77ef08829dfaad2f1ead7fd31fc9ff23
Esteban Zepeda <estzepeda@outlook.com>
2025-03-22 23:33:59 -0600

    Change Home.php to index.php

--- app.php
+++ app.php
@@ -1,8 +1,3 @@
-<?php
-    $base = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    $segments = count(array_filter(explode('/', $base)));
-    $path = str_repeat('../', $segments);
-?>
 <?php include("src/components/Navbar/Navbar.php");?>
 <main>
     <?php
@@ -10,7 +5,7 @@
 
         switch ($page) {
             case '/':
-                include('src/pages/Home/Home.php');
+                include('src/pages/Home');
                 break;
             default:
                 http_response_code(404);
@@ -20,6 +15,5 @@
     ?>
 </main>
 <?php include("src/components/Footer/Footer.php");?>
-<!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
