df8837248bf84f91952aca9f2ef13dd4cb359a6a
Esteban Zepeda <estzepeda@outlook.com>
2025-03-22 23:05:53 -0600

    Update app.php

--- app.php
+++ app.php
@@ -3,14 +3,14 @@
     $segments = count(array_filter(explode('/', $base)));
     $path = str_repeat('../', $segments);
 ?>
-<?php include("src/components/Navbar.php");?>
+<?php include("src/components/Navbar/Navbar.php");?>
 <main>
     <?php
         $page = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
         switch ($page) {
             case '/':
-                include('src/pages/Home.php');
+                include('src/pages/Home/Home.php');
                 break;
             default:
                 http_response_code(404);
@@ -19,7 +19,7 @@
         }
     ?>
 </main>
-<?php include("src/components/Footer.php");?>
+<?php include("src/components/Footer/Footer.php");?>
 <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
