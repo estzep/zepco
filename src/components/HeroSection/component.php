237700d58798bbdd750db7862197ca3f593aa5ae
Esteban Zepeda <estzepeda@outlook.com>
2025-03-29 18:50:05 -0600

    Testing HeroSection

---
+++ src/components/HeroSection/component.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="src/components/HeroSection/styles.css">
+
+<section class="heroSection">
+
+    <div id="heroSectionCarousel" class="carousel slide">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#heroSectionCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#heroSectionCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#heroSectionCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="src/assets/img/heroSection1.png" class="d-block w-100" alt="Hero Section 1">
+                <div class="container">
+                    <div class="content">
+                        Hero Section
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="src/assets/img/heroSection1.png" class="d-block w-100" alt="Hero Section 1">
+            </div>
+            <div class="carousel-item">
+                <img src="src/assets/img/heroSection1.png" class="d-block w-100" alt="Hero Section 1">
+            </div>
+        </div>
+    </div>
+</section>
--- src/pages/Home/page.php
+++ src/pages/Home/page.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="src/pages/Home/styles.css">
 
+<?php include("src/components/HeroSection/component.php");?>
 <section class="home">
     <div class="container">
         <div class="content">
