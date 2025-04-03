f1825e1a41b41213a4728ce8387e606afac2012f
Esteban Zepeda <estzepeda@outlook.com>
2025-04-03 13:48:56 -0600

    Test

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+    $base = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $segments = count(array_filter(explode('/', $base)));
+    $path = str_repeat('../', $segments);
+?>
+
 <!DOCTYPE html>
 <html lang="es">
     <?php require("src/components/Head/component.php");?>
--- src/pages/Team/page.php
+++ src/pages/Team/page.php
@@ -1,9 +1,14 @@
+<?php define('ROOT_PATH', dirname(__DIR__) . '/'); ?>
+
 <link rel="stylesheet" href="src/pages/Team/styles.css">
 
 <section id="team" class="team">
     <div class="container">
         <div class="content">
             Team
+            <?php echo $path; ?>
+            <br>
+            <?php echo ROOT_PATH; ?>
         </div>
     </div>
 </section> 
