f893594d9e14403a5d671cc3279ec8d2f6240a3a
Esteban Zepeda <estzepeda@outlook.com>
2025-04-03 17:37:17 -0600

    Update HeroSection

--- src/components/HeroSection/component.php
+++ src/components/HeroSection/component.php
@@ -3,8 +3,8 @@
 <section id="home" class="heroSection">
     <div id="heroSectionCarousel" class="carousel slide">
         <div class="carousel-indicators">
-            <button type="button" data-bs-target="#heroSectionCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
             <button type="button" data-bs-target="#heroSectionCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#heroSectionCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
             <button type="button" data-bs-target="#heroSectionCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
         <div class="carousel-inner">
@@ -12,15 +12,25 @@
                 <img src="/src/assets/img/heroSection1.png" alt="Hero Section 1">
                 <div class="container">
                     <div class="content">
-                        Hero Section
+                        Hero Section 1
                     </div>
                 </div>
             </div>
             <div class="carousel-item">
                 <img src="/src/assets/img/heroSection1.png" alt="Hero Section 1">
+                <div class="container">
+                    <div class="content">
+                        Hero Section 2
+                    </div>
+                </div>
             </div>
             <div class="carousel-item">
                 <img src="/src/assets/img/heroSection1.png" alt="Hero Section 1">
+                <div class="container">
+                    <div class="content">
+                        Hero Section 3
+                    </div>
+                </div>
             </div>
         </div>
     </div>
