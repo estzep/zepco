80f0409032c6813abca13a527ba13b64099660af
Esteban Zepeda <estzepeda@outlook.com>
2025-04-03 14:03:16 -0600

    Tst

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
-<?php
-    $base = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    $segments = count(array_filter(explode('/', $base)));
-    $path = str_repeat('../', $segments);
-?>
-
 <!DOCTYPE html>
 <html lang="es">
-    <?php require("/src/components/Head/component.php");?>
+    <?php require("src/components/Head/component.php");?>
 	<body>
-        <?php require("/app.php");?>
+        <?php require("app.php");?>
 	</body>
 </html>
