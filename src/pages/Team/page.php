30421108e7a84297c440f4af56aa9645e00bdc5a
Esteban Zepeda <estzepeda@outlook.com>
2025-04-03 13:53:25 -0600

    Test

--- src/pages/Team/page.php
+++ src/pages/Team/page.php
@@ -1,6 +1,6 @@
 <?php define('ROOT_PATH', dirname(__DIR__) . '/'); ?>
 
-<link rel="stylesheet" href="src/pages/Team/styles.css">
+<link rel="stylesheet" href="./src/pages/Team/styles.css">
 
 <section id="team" class="team">
     <div class="container">
