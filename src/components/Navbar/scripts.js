001eb2a33222e573f99aa623fc4ca6db51303cf2
Esteban Zepeda <estzepeda@outlook.com>
2025-04-03 19:22:23 -0600

    Update scripts.js

--- src/components/Navbar/scripts.js
+++ src/components/Navbar/scripts.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navbar = document.getElementById('navbar');
-    console.log(navbar);
     
     window.addEventListener('scroll', function() {
-        console.log(window.scrollY);
         if (window.scrollY >= 50) {
             navbar.classList.add('scrolled');
         } else {
