52afbeaf23adf0a77b3ad358aed4e6597ecc0817
Esteban Zepeda <estzepeda@outlook.com>
2025-04-03 18:30:58 -0600

    Navbar

--- src/components/Navbar/component.php
+++ src/components/Navbar/component.php
@@ -3,7 +3,30 @@
 <nav class="navbar">
     <div class="container">
         <div class="content">
-            Navbar
+            <div class="logo">
+                <a href="/">
+                    <img src="/src/assets/images/logo.png" alt="Zepeda & Asociados Abogados">
+                </a>
+            </div>
+            <div class="links">
+                <ul>
+                    <li class="active">
+                        <a href="/">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="/">Nosotros</a>
+                    </li>
+                    <li>
+                        <a href="/">Servicios Jurídicos</a>
+                    </li>
+                    <li>
+                        <a href="/team">Equipo</a>
+                    </li>
+                    <li>
+                        <a href="/">Contacto</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </nav>
