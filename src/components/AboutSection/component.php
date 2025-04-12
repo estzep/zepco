6147ae6fde2ef9a628ad1650bf71f6eacd7190a7
Esteban Zepeda <estzepeda@outlook.com>
2025-04-12 12:37:29 -0600

    Update component.php

--- src/components/AboutSection/component.php
+++ src/components/AboutSection/component.php
@@ -3,7 +3,19 @@
 <section id="about" class="aboutSection">
     <div class="container">
         <div class="content">
-            About Section
+            <div class="text">
+                <h2>Bienvenido a Zepeda Torres Moreno & Asociados</h2>
+                <p>
+                    Zepeda, Torres, Moreno & Asociados fue fundado en 1975, y desde entonces ofrece servicios legales integrales en México, combinando asesoría estratégica y litigio de alta especialización. Nuestro equipo cuenta con amplia experiencia en la planeación legal de proyectos inmobiliarios y corporativos, tanto a nivel nacional como internacional, incluyendo jurisdicciones de Common Law.
+                    <br>
+                    Representamos a empresas, inversionistas y personas físicas en la toma de decisiones clave, mitigando riesgos legales y resolviendo controversias con soluciones innovadoras. Nuestra firma destaca por su enfoque multidisciplinario y por defender los intereses de nuestros clientes ante todas las instancias, con respaldo de una red internacional de despachos aliados.
+                </p>
+                <a href="/contacto" class="btnType1">Ponte en contacto</a>
+            </div>
+            <div class="image">
+                <img src="/src/assets/img/about-section-image.png" alt="Zepeda Torres Moreno & Asociados">
+            </div>
+            <div class="square" />
         </div>
     </div>
 </section>
