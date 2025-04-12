e539a9636d0dc83408dc3f512570044f25d8d13f
Esteban Zepeda <estzepeda@outlook.com>
2025-04-12 17:56:31 -0600

    Update About Section

--- src/components/AboutSection/component.php
+++ src/components/AboutSection/component.php
@@ -10,12 +10,12 @@
                     <br>
                     Representamos a empresas, inversionistas y personas físicas en la toma de decisiones clave, mitigando riesgos legales y resolviendo controversias con soluciones innovadoras. Nuestra firma destaca por su enfoque multidisciplinario y por defender los intereses de nuestros clientes ante todas las instancias, con respaldo de una red internacional de despachos aliados.
                 </p>
-                <a href="/contacto" class="btnType1">Ponte en contacto</a>
+                <a href="/contacto" class="btn btnType1">Ponte en contacto</a>
             </div>
             <div class="image">
                 <img src="/src/assets/img/about-section-image.png" alt="Zepeda Torres Moreno & Asociados">
             </div>
-            <div class="square" />
         </div>
     </div>
+    <div class="square" />
 </section>
