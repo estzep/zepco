8dca0a339e1576c9cd4d96135e0923550277a2ec
Esteban Zepeda <estzepeda@outlook.com>
2025-05-06 13:45:20 -0600

    Update Home Page

--- src/pages/Home/page.php
+++ src/pages/Home/page.php
@@ -1,14 +1,5 @@
-<link rel="stylesheet" href="/src/pages/Home/styles.css">
-
 <?php include("src/components/HeroSection/component.php");?>
 <?php include("src/components/AboutSection/component.php");?>
 <?php include("src/components/ServicesSection/component.php");?>
-<section class="home">
-    <div class="container">
-        <div class="content">
-            Home
-        </div>
-    </div>
-</section> 
 <?php include("src/components/ValuesSection/component.php");?>
 <?php include("src/components/ContactSection/component.php");?>
