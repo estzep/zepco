9b882d7169789239a225c40cb97c8aa01043f77c
Esteban Zepeda <estzepeda@outlook.com>
2025-05-06 13:43:55 -0600

    Contact Section

--- src/components/ContactSection/component.php
+++ src/components/ContactSection/component.php
@@ -31,6 +31,26 @@
                 </form>
             </div>
             <div class="map">
+                <div class="info">
+                    <div class="infoItem">
+                        <p>
+                            <a href="https://maps.app.goo.gl/v7cAvqZ8Co9GmCnY6" target="_blank">
+                                <img src="/src/assets/icons/location.svg" alt="location" class="icon">
+                                Calle Bahía de la Concepción #14 Cuarto Piso, Colonia Verónica Anzures, 11300, Ciudad de México
+                            </a>
+                        </p>
+                    </div>
+                    <div class="infoItem">
+                        <p> 
+                            <a href="tel:+5255214242342"><img src="/src/assets/icons/phone.svg" alt="phone" class="icon">+52 (55) 2142-42342</a>
+                        </p>
+                        <p>
+                            <a href="mailto:info@zepco.com.mx"><img src="/src/assets/icons/mail.svg" alt="email" class="icon">info@zepco.com.mx</a>
+                        </p>
+                    </div>
+                    
+                </div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.503687881001!2d-99.1757880246867!3d19.433838740641214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f8b482724999%3A0x1108f5ae786e277b!2sZepeda%20Abogados%20S.C.!5e0!3m2!1ses-419!2smx!4v1746557507662!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </div>
