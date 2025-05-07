bb96eced602fb120bf6ecb22629a4d58d292e756
Esteban Zepeda <estzepeda@outlook.com>
2025-05-07 16:38:02 -0600

    Ud

--- src/components/ContactSection/component.php
+++ src/components/ContactSection/component.php
@@ -3,33 +3,30 @@
 <section id="contact" class="contactSection">
     <div class="container">
         <div class="content">
-            <div class="form">
-                <h2>Ponte en Contacto</h2>
-                <p class="description">Para contactarnos, llena el formulario y nos pondremos en contacto a la brevedad o bien llama al (55) 5260-6244.</p>
-
-                <form action="">
-                    <div class="formGroup">
-                        <label for="name">Nombre y apellido</label>
-                        <input id="name" name="name" type="text" placeholder="Ingresa tu nombre">
-                    </div>
-                    <div class="formGroup">
-                        <label for="email">Correo electrónico</label>
-                        <input id="email" name="email" type="email" placeholder="Ingresa tu correo electrónico">
-                    </div>
-                    <div class="formGroup">
-                        <label for="tel">Teléfono</label>
-                        <input id="tel" name="tel" type="tel" placeholder="Ingresa tu teléfono">
-                    </div>
-                    <div class="formGroup">
-                        <label for="message">Mensaje</label>
-                        <textarea id="message" name="message" placeholder="Ingresa tu mensaje" rows="4"></textarea>
-                    </div>
-                    <div class="formGroup">
-                        <button type="submit" class="btn btnType1">Enviar</button>
-                    </div>
-                    
-                </form>
-            </div>
+            <h2>Ponte en Contacto</h2>
+            <p class="description">Para contactarnos, llena el formulario y nos pondremos en contacto a la brevedad o bien llama al (55) 5260-6244.</p>
+            <form action="">
+                <div class="formGroup">
+                    <label for="name">Nombre y apellido</label>
+                    <input id="name" name="name" type="text" placeholder="Ingresa tu nombre">
+                </div>
+                <div class="formGroup">
+                    <label for="email">Correo electrónico</label>
+                    <input id="email" name="email" type="email" placeholder="Ingresa tu correo electrónico">
+                </div>
+                <div class="formGroup">
+                    <label for="tel">Teléfono</label>
+                    <input id="tel" name="tel" type="tel" placeholder="Ingresa tu teléfono">
+                </div>
+                <div class="formGroup">
+                    <label for="message">Mensaje</label>
+                    <textarea id="message" name="message" placeholder="Ingresa tu mensaje" rows="4"></textarea>
+                </div>
+                <div class="formGroup">
+                    <button type="submit" class="btn btnType1">Enviar</button>
+                </div>
+                
+            </form>
             <div class="map">
                 <div class="info">
                     <div class="infoItem">
