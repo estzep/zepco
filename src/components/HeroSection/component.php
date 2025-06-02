5e069a9411514c2c9f7b2263ab1bad614ba58cdf
Esteban Zepeda <estzepeda@outlook.com>
2025-06-02 17:56:23 -0600

    Update HeroSection

--- src/components/HeroSection/component.php
+++ src/components/HeroSection/component.php
@@ -1,38 +1,11 @@
 <link rel="stylesheet" href="/src/components/HeroSection/styles.css">
 
 <section id="home" class="heroSection">
-    <div id="heroSectionCarousel" class="carousel slide">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#heroSectionCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#heroSectionCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#heroSectionCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="/src/assets/img/heroSection1.png" alt="Hero Section 1">
-                <div class="container">
-                    <div class="content">
-                        <h1>Brindando servicios profesionales de abogacía en México</h1>
-                        <p class="subtitle">Con más de 48 años de experiencia asesorando, protegiendo y representando a nuestros clientes.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="/src/assets/img/heroSection1.png" alt="Hero Section 1">
-                <div class="container">
-                    <div class="content">
-                        Hero Section 2
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="/src/assets/img/heroSection1.png" alt="Hero Section 1">
-                <div class="container">
-                    <div class="content">
-                        Hero Section 3
-                    </div>
-                </div>
-            </div>
+    <img src="/src/assets/img/heroSection1.png" alt="Hero Section 1">
+    <div class="container">
+        <div class="content">
+            <h1>Brindando servicios profesionales de abogacía en México</h1>
+            <p class="subtitle">Con más de 48 años de experiencia asesorando, protegiendo y representando a nuestros clientes.</p>
         </div>
     </div>
 </section>
