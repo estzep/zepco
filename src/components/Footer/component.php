847cb9311706a755d9798534883a8a3fb56df71d
Esteban Zepeda <estzepeda@outlook.com>
2025-06-02 19:04:04 -0600

    Update component.php

--- src/components/Footer/component.php
+++ src/components/Footer/component.php
@@ -4,28 +4,28 @@
     <div class="container">
         <div class="content">
             <div class="about">
-                <a href="/"><img src="/src/assets/img/full-zepco-logo.svg" alt="Logo de Zepco"></a>
+                <a href="/#home"><img src="/src/assets/img/full-zepco-logo.svg" alt="Logo de Zepco"></a>
                 <p>Somos un despacho de abogados en Ciudad de México con más de 48 años de experiencia en temas de litigio.</p>
             </div>
             <div class="link-columns">
                 <div class="link-column">
                     <h3>Navegación</h3>
                     <ul>
-                        <li><a href="/">Inicio</a></li>
-                        <li><a href="/">Nosotros</a></li>
-                        <li><a href="/">Servicios Jurídicos</a></li>
-                        <li><a href="/">Equipo</a></li>
-                        <li><a href="/">Contacto</a></li>
+                        <li><a href="/#home">Inicio</a></li>
+                        <li><a href="/#about">Nosotros</a></li>
+                        <li><a href="/#services">Servicios Jurídicos</a></li>
+                        <li><a href="/team">Equipo</a></li>
+                        <li><a href="/#contact">Contacto</a></li>
                     </ul>
                 </div>
                 <div class="link-column">
                     <h3>Servicios Jurídicos</h3>
                     <ul>
-                        <li><a href="/">Derecho Inmobiliario</a></li>
-                        <li><a href="/">Propiedad Intelectual</a></li>
-                        <li><a href="/">Litigio en Materia Civil, Mercantil, Familiar y Corporativo</a></li>
-                        <li><a href="/">Investigación de Títulos de Propiedad</a></li>
-                        <li><a href="/">Protección Matrimonial</a></li>
+                        <li><a>Derecho Inmobiliario</a></li>
+                        <li><a>Propiedad Intelectual</a></li>
+                        <li><a>Litigio en Materia Civil, Mercantil, Familiar y Corporativo</a></li>
+                        <li><a>Investigación de Títulos de Propiedad</a></li>
+                        <li><a>Protección Matrimonial</a></li>
                     </ul>
                 </div>
                 <div class="link-column">
@@ -33,7 +33,7 @@
                     <ul>
                         <li><a href="mailto:info@zepco.com.mx"><img src="/src/assets/icons/mail.svg" alt="Correo electrónico" class="icon">info@zepco.com.mx</a></li>
                         <li><a href="tel:+5255214242342"><img src="/src/assets/icons/phone.svg" alt="Teléfono" class="icon">+52 (55) 2142-42342</a></li>
-                        <li><a href="/"><img src="/src/assets/icons/map-pin.svg" alt="Ubicación" class="icon">Calle Bahía de la Concepción #14 Cuarto Piso, Colonia Verónica Anzures, 11300, Ciudad de México</a></li>
+                        <li><a href="https://maps.app.goo.gl/XER4Q3CTpkLnfZWn6" target="_blank"><img src="/src/assets/icons/map-pin.svg" alt="Ubicación" class="icon">Calle Bahía de la Concepción #14 Cuarto Piso, Colonia Verónica Anzures, 11300, Ciudad de México</a></li>
                     </ul>
                 </div>
             </div>
