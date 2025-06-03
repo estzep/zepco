1077e9144e99434d1548f8bb8b57b40505fcb5ba
Esteban Zepeda <estzepeda@outlook.com>
2025-06-02 19:58:39 -0600

    Corrections

--- src/components/AboutSection/component.php
+++ src/components/AboutSection/component.php
@@ -5,12 +5,16 @@
         <div class="content">
             <div class="text">
                 <h2>
-                    Bienvenido a Zepeda Torres
+                    Bienvenido a Zepeda, Torres,
                     <br>
                     Moreno & Asociados
                 </h2>
                 <p>
-                    Zepeda, Torres, Moreno & Asociados fue fundado en 1975, y desde entonces ofrece servicios legales integrales en México, combinando asesoría estratégica y litigio de alta especialización. Nuestro equipo cuenta con amplia experiencia en la planeación legal de proyectos inmobiliarios y corporativos, tanto a nivel nacional como internacional, incluyendo jurisdicciones de Common Law.
+                    La fundación de Zepeda, Torres, Moreno & Asociados se remonta al año de 1975, formalmente bajo el nombre de Bufete Zepeda Trujillo, a partir del cual comienza a prestar servicios profesionales de abogacía, tanto en su aspecto de asesoría en materia de planeación legal, negociaciones, contrataciones e investigaciones, como de litigio.
+                    <br>
+                    <br>
+                    Nuestro equipo cuenta con amplia experiencia en la planeación legal de proyectos inmobiliarios y corporativos, tanto a nivel nacional como internacional, incluyendo jurisdicciones de Common Law.
+                    <br>
                     <br>
                     Representamos a empresas, inversionistas y personas físicas en la toma de decisiones clave, mitigando riesgos legales y resolviendo controversias con soluciones innovadoras. Nuestra firma destaca por su enfoque multidisciplinario y por defender los intereses de nuestros clientes ante todas las instancias, con respaldo de una red internacional de despachos aliados.
                 </p>
--- src/components/ContactBar/component.php
+++ src/components/ContactBar/component.php
@@ -3,11 +3,11 @@
 <div id="contactBar" class="contactBar">
     <div class="container">
         <div class="content">
-            <div class="hours">
+            <!-- <div class="hours">
                 <p>
                     <img src="/src/assets/icons/clock.svg" alt="clock" class="icon">Horario de atención Lunes - Viernes | 9:00 hrs - 19:00 hrs
                 </p>
-            </div>
+            </div> -->
             <div class="contacts">
                 <p class="phone">
                     <a href="tel:+5255214242342"><img src="/src/assets/icons/phone.svg" alt="phone" class="icon">+52 (55) 2142-42342</a>
