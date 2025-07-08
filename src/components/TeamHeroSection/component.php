8253710bb993db115c94a4f203e713da4049e81e
Esteban Zepeda <estzepeda@outlook.com>
2025-07-08 17:30:29 -0600

    update

--- src/components/TeamHeroSection/component.php
+++ src/components/TeamHeroSection/component.php
@@ -4,12 +4,8 @@
     <img src="/src/assets/img/heroSection2.png" alt="Hero Section 2">
     <div class="container">
         <div class="content">
-            <div class="column">
-                <h3>Somos un equipo profesional comprometido con brindarte un servicio legal de excelencia</h3>
-            </div>
-            <div class="column">
-                <p class="subtitle">Desde 1970, hemos formado un equipo sólido y multidisciplinario que combina experiencia, colaboración y altos estándares de calidad para brindar un servicio legal confiable y comprometido en cada asunto.</p>
-            </div>
+            <h3>Somos un equipo profesional comprometido con brindarte un servicio legal de excelencia</h3>
+            <p class="subtitle">Desde 1970, hemos formado un equipo sólido y multidisciplinario que combina experiencia, colaboración y altos estándares de calidad para brindar un servicio legal confiable y comprometido en cada asunto.</p>
         </div>
     </div>
 </section>
