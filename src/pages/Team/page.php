390f58bab564e96340f0b71fde222f22418314aa
Esteban Zepeda <estzepeda@outlook.com>
2025-07-08 18:28:41 -0600

    Update

--- src/pages/Team/page.php
+++ src/pages/Team/page.php
@@ -1,11 +1,31 @@
+<?php
+$team = json_decode(file_get_contents(__DIR__ . '/../../data/team.json'), true);
+$team = $team['team'] ?? [];
+?>
+
 <link rel="stylesheet" href="/src/pages/Team/styles.css">
 
 <?php include("src/components/TeamHeroSection/component.php");?>
 <section id="team" class="team">
     <div class="container">
         <div class="content">
-            Team
+            <h4>Socios</h4>
+            <div class="teamMembers">
+                <?php foreach ($team as $member): ?>
+                <div class="teamMember">
+                    <div class="image">
+                        <img src="<?php echo $member['image']; ?>" alt="<?php echo $member['name']; ?>">
+                    </div>
+                    <div class="info">
+                        <h5><?php echo $member['name']; ?></h5>
+                        <p class="position"><?php echo $member['position']; ?></p>
+                        <p class="description"><?php echo $member['description']; ?></p>
+                        <a href="/team/<?php echo $member['username']; ?>">Más información</a>
+                    </div>
+                </div>
+                <?php endforeach; ?>
+            </div>
         </div>
     </div>
-</section> 
+</section>
 <?php include("src/components/ContactSection/component.php");?>
