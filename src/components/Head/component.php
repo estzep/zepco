f461372774ac29ed8bbfb780cbd8757a971d1b65
Diego Zepeda <109043513+estzep@users.noreply.github.com>
2026-04-20 15:56:43 -0600

    update

--- src/components/Head/component.php
+++ src/components/Head/component.php
@@ -23,7 +23,7 @@ $robots = $metadata[$page]['robots'] ?? 'noindex, nofollow';
     <meta name="url" content="https://zepco.mx<?php echo $page; ?>">
     
     <!-- Favicon and icons -->
-    <link rel="icon" href="./favicon.ico">
+    <link rel="icon" href="/favicon.ico">
 
     <!-- Stylesheets -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
